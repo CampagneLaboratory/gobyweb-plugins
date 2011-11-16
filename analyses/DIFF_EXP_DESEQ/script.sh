43b0800c9ff97f6e6412094648f33205c839e61e
Fabien Campagne <fac2003@campagnelab.org>
2011-11-16 17:15:05 +0000

    Add a validation rule to chekc that a formula is provided when  GC content adjustment is selected.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@29481 60cd2ccc-2b05-0410-9566-da273a940bff

--- analyses/DIFF_EXP_DESEQ/script.sh
+++ analyses/DIFF_EXP_DESEQ/script.sh
@@ -42,7 +42,63 @@
 # USE_WEIGHTS_DIRECTIVE = optional, command line flags to have Goby annotation-to-counts adjust counts with weigths.
 
 function eval {
-EVAL=counts
+EVAL=raw-counts
+}
+
+function setupWeights {
+
+   if [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_WEIGHT_ADJUSTMENT}" == "NONE" ]; then
+
+       USE_WEIGHTS_DIRECTIVE=" "
+
+   elif [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_WEIGHT_ADJUSTMENT}" == "GC_CONTENT" ]; then
+
+       USE_WEIGHTS_DIRECTIVE="--use-weights gc --adjust-gc-bias ${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_BIAS_ADJUSTMENT_FORMULA} "
+
+   elif [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_WEIGHT_ADJUSTMENT}" == "HEPTAMERS" ]; then
+
+       USE_WEIGHTS_DIRECTIVE="--use-weights heptamers "
+   else
+     dieUponError "weight adjustment  not supported: ${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_WEIGHT_ADJUSTMENT}"
+   fi
+
+}
+
+function setupAnnotationTypes {
+   ANNOTATION_TYPES=""
+   if [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_ESTIMATE_COUNTS_GENE}" == "true" ]; then
+
+       ANNOTATION_TYPES="${ANNOTATION_TYPES}gene"
+   fi
+   if [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_ESTIMATE_COUNTS_EXON}" == "true" ]; then
+       if [ "${ANNOTATION_TYPES}" != "" ]; then
+          ANNOTATION_TYPES="${ANNOTATION_TYPES},"
+       fi
+       ANNOTATION_TYPES="${ANNOTATION_TYPES}exon"
+   fi
+   if [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_ESTIMATE_COUNTS_OTHER}" == "true" ]; then
+       if [ "${ANNOTATION_TYPES}" != "" ]; then
+          ANNOTATION_TYPES="${ANNOTATION_TYPES},"
+       fi
+       ANNOTATION_TYPES="${ANNOTATION_TYPES}other"
+   fi
+
+   if [ "${ANNOTATION_TYPES}" == "" ]; then
+     dieUponError "At least one annotation type must be selected to run a differential analysis."
+   fi
+
+}
+
+function setupAnnotationSource {
+
+  ANNOTATION_SOURCE=""
+  if [ "${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_ANNOTATION_SOURCE}" == "GENE_EXON_OTHER" ]; then
+    # gene exon annotation file.
+    ANNOTATION_SOURCE="${REFERENCE_DIRECTORY}/exon-annotations.tsv"
+  else
+    # CNV annotation file.
+    ANNOTATION_SOURCE="${REFERENCE_DIRECTORY}/cnv-annotations.tsv"
+  fi
 }
 
 . ${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_FILES_PARALLEL_SCRIPT}
@@ -54,10 +110,10 @@ function plugin_alignment_analysis_combine {
    shift
    PART_RESULT_FILES=$*
 
-   NUM_TOP_HITS=${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_GOBY_NUM_TOP_HITS}
-   Q_VALUE_THRESHOLD=${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_GOBY_Q_VALUE_THRESHOLD}
+   NUM_TOP_HITS=${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_NUM_TOP_HITS}
+   Q_VALUE_THRESHOLD=${PLUGINS_ALIGNMENT_ANALYSIS_DIFF_EXP_DESEQ_Q_VALUE_THRESHOLD}
 
-   # following function from DIFF_EXP_GOBY/parallel.sh
+   # following function from DIFF_EXP_DESEQ/parallel.sh
    run_fdr
 
    if [ $? -eq 0 ]; then
