3af29d0cee241c0496c9acd1a255b1f6fd491716
Fabien Campagne <fac2003@campagnelab.org>
2011-11-20 00:20:13 +0000

    Make sure POST align has enough memory. Push version of goby with run-parallel that wait on "scanning". Scan reverse strand and forward strands.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@29618 60cd2ccc-2b05-0410-9566-da273a940bff

--- aligners/LAST_BISULFITE/align-bisulfite-reverse.sh
+++ aligners/LAST_BISULFITE/align-bisulfite-reverse.sh
@@ -8,12 +8,12 @@ JOB_DIR=$5
 . ${JOB_DIR}/constants.sh
 . ${JOB_DIR}/auto-options.sh
 set -x
-  ${RESOURCES_LAST_EXEC_PATH} -v -p ${RESOURCES_LAST_BISULFITE_REVERSE_MATRIX} -s1 -Q1 -d${PLUGINS_ALIGNER_LAST_BISULFITE_D} \
-             -e${PLUGINS_ALIGNER_LAST_BISULFITE_E} ${INDEX_DIRECTORY}/index_r ${READS_FASTQ} -o ${TEMP_FILENAME1}.maf
+  ${RESOURCES_LAST_EXEC_PATH} -v -p ${RESOURCES_LAST_BISULFITE_REVERSE_MATRIX} -s0 -Q1 -d${PLUGINS_ALIGNER_LAST_BISULFITE_D} \
+             -e${PLUGINS_ALIGNER_LAST_BISULFITE_E} ${INDEX_DIRECTORY}/index_r ${READS_FASTQ} -o r-${TEMP_FILENAME1}.maf
 
   java ${GRID_JVM_FLAGS} -Dlog4j.debug=true -Dlog4j.configuration=file:${JOB_DIR}/goby/log4j.properties \
                        -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_GOBY_GOBY_JAR} \
-                       --mode  last-to-compact  --flip-strand -i ${TEMP_FILENAME1} -o ${OUTPUT} --third-party-input true \
+                       --mode  last-to-compact  --flip-strand -i r-${TEMP_FILENAME1} -o ${OUTPUT} --third-party-input true \
                        --only-maf -q ${FULL_READS_INPUT} -t ${REFERENCE} --quality-filter-parameters threshold=1.0
 
