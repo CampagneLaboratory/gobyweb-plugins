f35317da9d2eeaf8868f2591e1acc6111612351f
Fabien Campagne <fac2003@campagnelab.org>
2011-11-29 17:43:53 +0000

    Set minimum JVM memory at max so that jobs fail early if they have to. This should prevent errors such as below from occurring:
    # An unexpected error has been detected by Java Runtime Environment:
    #
    # java.lang.OutOfMemoryError: requested 32768000 bytes for GrET in /BUILD_AREA/jdk6_02/hotspot/src/share/vm/utilities/growableArray.cpp. Out of swap space?
    #
    #  Internal Error (414C4C4F434154494F4E0E494E4C494E450E4850500017), pid=20072, tid=1098635584
    #
    # Java VM: Java HotSpot(TM) 64-Bit Server VM (1.6.0_02-b05 mixed mode)
    # An error report file with more information is saved as hs_err_pid20072.log
    
    Workaround is to not do heap resizing - set -Xmx & -Xms to the same value. The error has not reproduced since then.
    
    
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@29866 60cd2ccc-2b05-0410-9566-da273a940bff

--- resources/GOBY_DEV/goby-script.sh
+++ resources/GOBY_DEV/goby-script.sh
@@ -39,8 +39,8 @@ function run-goby {
    shift
    mode_name="$1"
    shift
-
-   java -Xmx${memory} -Dlog4j.debug=true -Dlog4j.configuration=file:${TMPDIR}/log4j.properties \
+   # set both minimum and max right away so failure occurs early and parallel GC is happier.
+   java -Xms${memory} -Xmx${memory} -Dlog4j.debug=true -Dlog4j.configuration=file:${TMPDIR}/log4j.properties \
                                              -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_GOBY_GOBY_JAR} \
                        --mode ${mode_name} $*
@@ -52,7 +52,7 @@ function goby {
    set -T
    mode_name="$1"
    shift
-
+     # set both minimum and max right away so failure occurs early and parallel GC is happier.
    java ${GRID_JVM_FLAGS} -Dlog4j.debug=true -Dlog4j.configuration=file:${TMPDIR}/log4j.properties \
                                              -Dgoby.configuration=file:${TMPDIR}/goby.properties \
                        -jar ${RESOURCES_GOBY_GOBY_JAR} \
