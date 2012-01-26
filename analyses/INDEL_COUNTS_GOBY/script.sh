ab8aafd4ed832b122b25ffe8ca75192fd4430c7c
Fabien Campagne <fac2003@campagnelab.org>
2012-01-26 13:23:08 +0000

    Draft attempt at parallelization of indel-counts plugin.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@30592 60cd2ccc-2b05-0410-9566-da273a940bff

--- analyses/INDEL_COUNTS_GOBY/script.sh
+++ analyses/INDEL_COUNTS_GOBY/script.sh
@@ -50,6 +50,93 @@
 
 . ${RESOURCES_GOBY_SHELL_SCRIPT}
 
+function plugin_alignment_analysis_split {
+
+  NUMBER_OF_PARTS=$1
+  SPLICING_PLAN_RESULT=$2
+  shift
+  shift
+  goby suggest-position-slices \
+          --number-of-slices ${NUMBER_OF_PARTS} \
+          --output ${SPLICING_PLAN_RESULT} \
+          $*
+}
+
+
+# This function return the number of parts in the slicing plan. It returns zero if the alignments could not be split.
+function plugin_alignment_analysis_num_parts {
+   SPLICING_PLAN_FILE=$1
+
+   if [ $? -eq 0 ]; then
+
+        return `grep -v targetIdStart ${SPLICING_PLAN_FILE} | wc -l `
+   fi
+
+   return 0
+}
+
+function plugin_alignment_analysis_process {
+   SLICING_PLAN_FILENAME=$1
+   ARRAY_JOB_INDEX=$2
+   shift
+   shift
+   MINIMUM_VARIATION_SUPPORT=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_MINIMUM_VARIATION_SUPPORT}
+   THRESHOLD_DISTINCT_READ_INDICES=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_THRESHOLD_DISTINCT_READ_INDICES}
+   OUTPUT_FORMAT=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_OUTPUT_FORMAT}
+
+   # These variables are defined: SLICING_PLAN_FILENAME
+     echo "Processing run_single_alignment_analysis_process for part ${SGE_TASK_ID}"
+
+     WINDOW_LIMITS=`awk -v arrayJobIndex=${ARRAY_JOB_INDEX} '{ if (lineNumber==arrayJobIndex) print " -s "$3" -e "$6; lineNumber++; }' ${SLICING_PLAN_FILENAME}`
+     STAT2_FILENAME=${SGE_O_WORKDIR}/results/${TAG}-variations-stats2.tsv
+
+     echo "Discovering sequence variants for window limits: ${WINDOW_LIMITS} and statsFilename: ${STAT2_FILENAME}"
+
+     ${QUEUE_WRITER} --tag ${TAG} --status ${JOB_PART_DIFF_EXP_STATUS} --description "Start discover-sequence-variations for part # ${CURRENT_PART}." --index ${CURRENT_PART} --job-type job-part
+     REALIGNMENT_OPTION=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_REALIGN_AROUND_INDELS}
+     if [ "${REALIGNMENT_OPTION}" == "true" ]; then
+
+            REALIGNMENT_ARGS=" --processor realign_near_indels "
+     else
+            REALIGNMENT_ARGS="  "
+     fi
+
+     # Note that we override the grid jvm flags to request only 4Gb:
+     run-goby ${PLUGIN_NEED_PROCESS_JVM} discover-sequence-variants \
+           ${WINDOW_LIMITS} \
+           --groups ${GROUPS_DEFINITION} \
+           --compare ${COMPARE_DEFINITION} \
+           --format ${OUTPUT_FORMAT} \
+           --eval filter \
+           ${REALIGNMENT_ARGS} \
+           --genome ${REFERENCE_DIRECTORY}/random-access-genome \
+           --minimum-variation-support ${MINIMUM_VARIATION_SUPPORT} \
+           --threshold-distinct-read-indices ${THRESHOLD_DISTINCT_READ_INDICES} \
+           --output ignored-filename  \
+           --call-indels true \
+           ${ENTRIES_FILES}
+
+     dieUponError  "Failed to count indels in alignment files, sub-task ${CURRENT_PART} failed."
+     cp indel-counts.tsv  ${TAG}-indel-counts-${ARRAY_JOB_INDEX}.tsv
+     ${QUEUE_WRITER} --tag ${TAG} --status ${JOB_PART_DIFF_EXP_STATUS} --description "End discover-sequence-variations for part # ${ARRAY_JOB_INDEX}." --index ${CURRENT_PART} --job-type job-part
+
+}
+
+function plugin_alignment_analysis_combine {
+
+   RESULT_FILE=stats.tsv
+   shift
+   PART_RESULT_FILES=$*
+
+
+   run-goby ${PLUGIN_NEED_COMBINE_JVM} fdr \
+          ${PART_RESULT_FILES}  \
+          --output ${TMPDIR}/${TAG}-indel-counts.tsv
+   dieUponError  "Failed to merge split results for indel counts."
+
+   cp ${TMPDIR}/${TAG}-indel-counts.tsv ${RESULT_FILE}
+
+}
 
 function plugin_alignment_analysis_sequential {
    OUTPUT_FORMAT=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_GOBY_OUTPUT_FORMAT}
@@ -61,7 +148,7 @@ function plugin_alignment_analysis_sequential {
    THRESHOLD_DISTINCT_READ_INDICES=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_GOBY_THRESHOLD_DISTINCT_READ_INDICES}
    OUTPUT_FORMAT=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_GOBY_OUTPUT_FORMAT}
 
-   REALIGNMENT_OPTION=${PLUGINS_ALIGNMENT_ANALYSIS_SEQ_VAR_GOBY_REALIGN_AROUND_INDELS}
+   REALIGNMENT_OPTION=${PLUGINS_ALIGNMENT_ANALYSIS_INDEL_COUNTS_REALIGN_AROUND_INDELS}
    if [ "${REALIGNMENT_OPTION}" == "true" ]; then
 
            REALIGNMENT_ARGS=" --processor realign_near_indels "
