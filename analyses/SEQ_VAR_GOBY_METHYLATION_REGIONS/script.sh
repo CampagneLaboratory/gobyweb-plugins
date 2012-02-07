23306b8ef0037edcb1a4e9fef92d030b423792fb
Fabien Campagne <fac2003@campagnelab.org>
2012-02-07 18:17:58 +0000

    report all results.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@30740 60cd2ccc-2b05-0410-9566-da273a940bff

--- analyses/SEQ_VAR_GOBY_METHYLATION_REGIONS/script.sh
+++ analyses/SEQ_VAR_GOBY_METHYLATION_REGIONS/script.sh
@@ -169,8 +169,9 @@ function plugin_alignment_analysis_combine {
    echo "Adjusting P-value columns: $COLUMNS"
    
    Q_VALUE_THRESHOLD=${PLUGINS_ALIGNMENT_ANALYSIS_SEQ_VAR_GOBY_METHYLATION_REGIONS_Q_VALUE_THRESHOLD}
-   # Remove this to filter by q-value threshold (when p-values have been added to the TSV)
+   # Remove the next two lines to filter by q-value threshold (when p-values have been added to the TSV)
    Q_VALUE_THRESHOLD=1.0
+   NUM_TOP_HITS=1000000000
    run-goby ${PLUGIN_NEED_COMBINE_JVM} fdr \
           --q-threshold ${Q_VALUE_THRESHOLD} \
           --top-hits ${NUM_TOP_HITS} \
