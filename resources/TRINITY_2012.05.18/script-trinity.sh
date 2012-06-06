967d759d4a511b562f953a29f6cb6c07fd123f11
Zachary Zeno <zmz2@cornell.edu>
2012-06-06 16:21:42 +0000

    Added run_trinity wrapper function to Trinity resource
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@32216 60cd2ccc-2b05-0410-9566-da273a940bff

--- resources/TRINITY_2012.05.18/script-trinity.sh
+++ resources/TRINITY_2012.05.18/script-trinity.sh
@@ -1,15 +1,40 @@
 
-#args list
-#1: Path to the reads file
-#2: Path to the alignment (basename)
-#3: Output file
-
 
 function setup_trinity {
 
-	tar -x -f trinity.tar
+	if [ ! -e Trinity.pl ]
+	then
+		tar -x -f ${RESOURCE_TRINITY_TRINITY_TAR}
+	fi
+	
+}
 
+#args list
+#1: Path to input compact-reads file
+#2: Output file
+#3: Other options for trinity
+
+function run_trinity {
+
+	local INPUT=$1
+	local OUTPUT=$2
+	local OTHER_OPTIONS=$3
+
+	#trinity doesnt support compact-reads, convert to fastq
+	local TEMPFILE='mktemp readsXXXX'
+	goby compact-to-fasta --output-format fastq --input ${INPUT} --output ${TEMPFILE}
+	
+	#run trinity on converted file
+	local TEMPDIR='mktemp -d trinity_outXXXX'
+	./Trinity.pl --seqType fq -kmer_method inchworm --single ${TEMPFILE} --CPU 4 --output ${TEMPDIR} ${OTHER_OPTIONS}
+	
+	#copy trinity output file to specified location
+	cp ${TEMPDIR}/Trinity.fasta ${OUTPUT}
+	
 }
 
 
 setup_trinity
+
+
+
