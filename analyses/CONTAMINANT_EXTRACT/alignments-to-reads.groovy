05e16b033c2529d2d04b53402f23ab421ab9132f
Kevin C. Dorf <kdorff@campagnelab.org>
2012-06-21 19:16:13 +0000

    * Plugins now support executing a script before as the job is being submitted to the cluster. The script is given a goby object and a temporary directory, anything written to that directory will be copied to the server for use during job execution. If the file that is written ends in ".sh" it will be made executable. After the files are copied to the server, the temporary directory will be removed.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@32338 60cd2ccc-2b05-0410-9566-da273a940bff

--- analyses/CONTAMINANT_EXTRACT/alignments-to-reads.groovy
+++
@@ -1,4 +0,0 @@
-// put some groovy code in here that receives two arguments:
-
-// arg[0]: object that GobyWeb just configured for this plugin
-// arg[1]: temporary directory where to write files that will be transferred to the cluster with the plugin
