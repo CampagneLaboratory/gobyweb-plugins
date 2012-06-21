d8466bec9b1980f827ab3d00841b497a16a95aed
Fabien Campagne <fac2003@campagnelab.org>
2012-06-21 15:17:11 +0000

    Add xml schema support for pre-deployment (web-server side) groovy scripts.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@32335 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ analyses/CONTAMINANT_EXTRACT/alignments-to-reads.groovy
@@ -0,0 +1,4 @@
+// put some groovy code in here that receives two arguments:
+
+// arg[0]: object that GobyWeb just configured for this plugin
+// arg[1]: temporary directory where to write files that will be transferred to the cluster with the plugin
