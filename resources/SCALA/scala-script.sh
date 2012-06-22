b83b37876a9eccd458e97eb870388a7cad471417
Fabien Campagne <fac2003@campagnelab.org>
2012-06-22 19:16:14 +0000

    Remove console output.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/gobyweb/server-conf/plugins@32344 60cd2ccc-2b05-0410-9566-da273a940bff

--- resources/SCALA/scala-script.sh
+++ resources/SCALA/scala-script.sh
@@ -1,13 +1,13 @@
 # #!/bin/sh
 # . constants.sh
 # . auto-options.sh
-set -x
+
 # This script will either find a global Scala installation matching the SCALA plugin version (via
 # the SCALA_HOME environment variable) or install the requested version in the SGE work directory.
 function install {
     local filename=`basename ${RESOURCES_SCALA_TGZ_ARCHIVE} .tgz`
     local VERSION="${filename#scala-}"
-    echo version="${VERSION}"
+    #echo version="${VERSION}"
     if [ -x "${SCALA_HOME}/bin/scala" ]; then
         # found existing SCALA_HOME installation with correct version
         return
@@ -20,7 +20,7 @@ function install {
     fi
     if [ -z "${SCALA_HOME+xxx}" ]; then
         # SCALA_HOME is not defined, we decompress the distribution
-        echo Installing Scala resource
+        #echo Installing Scala resource
         gzip -c -d ${RESOURCES_SCALA_TGZ_ARCHIVE}	| tar -x -f -
         SCALA_HOME="`pwd`/scala-${VERSION}/"
         export SCALA_HOME
@@ -39,6 +39,7 @@ function scala {
         local MEM=$1
         local CLASSPATH_SCALA=$2
         shift 2
+        export JAVA_HOME=/softlib/exe/x86_64/pkg/sun_jdk/6.0.2/dist
         ${SCALA_HOME}/bin/scala -J-Xmx${MEM} -cp ${CLASSPATH_SCALA} $*
      fi
 }
